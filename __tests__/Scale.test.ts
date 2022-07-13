e74552cb86065c65bd307c9a92260e560ab5a2cf
simonppg <simonppg@gmail.com>
2022-07-12 21:01:35 -0700

    test(scale): Test C# Mayor scale degrees and modes

--- __tests__/Scale.test.ts
+++ __tests__/Scale.test.ts
@@ -30,3 +30,32 @@ test('C Mayor scale degrees and modes', ()=>{
   expect(scale.mode(Modes.VII)).toStrictEqual(['B', 'C', 'D', 'E', 'F', 'G', 'A'])
   expect(scale.mode(Modes.VIII)).toStrictEqual(['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 })
+
+test('C# Mayor scale degrees and modes', ()=>{
+
+  const scale = new Scale('C#', Scales.mayor)
+
+  expect(scale.degree(Degrees.I)).toStrictEqual('C#')
+  expect(scale.degree(Degrees.II)).toStrictEqual('D#')
+  expect(scale.degree(Degrees.III)).toStrictEqual('F')
+  expect(scale.degree(Degrees.IV)).toStrictEqual('F#')
+  expect(scale.degree(Degrees.V)).toStrictEqual('G#')
+  expect(scale.degree(Degrees.VI)).toStrictEqual('A#')
+  expect(scale.degree(Degrees.VII)).toStrictEqual('C')
+  expect(scale.degree(Degrees.VIII)).toStrictEqual('C#')
+  expect(scale.degree(Degrees.IX)).toStrictEqual('D#')
+  expect(scale.degree(Degrees.X)).toStrictEqual('F')
+  expect(scale.degree(Degrees.XI)).toStrictEqual('F#')
+  expect(scale.degree(Degrees.XII)).toStrictEqual('G#')
+  expect(scale.degree(Degrees.XIII)).toStrictEqual('A#')
+
+  expect(scale.mode(Modes.I)).toStrictEqual(['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C'])
+  expect(scale.mode(Modes.II)).toStrictEqual(['D#', 'F', 'F#', 'G#', 'A#', 'C', 'C#'])
+  expect(scale.mode(Modes.III)).toStrictEqual(['F', 'F#', 'G#', 'A#', 'C', 'C#', 'D#'])
+  expect(scale.mode(Modes.IV)).toStrictEqual(['F#', 'G#', 'A#', 'C', 'C#', 'D#', 'F'])
+  expect(scale.mode(Modes.V)).toStrictEqual(['G#', 'A#', 'C', 'C#', 'D#', 'F', 'F#'])
+  expect(scale.mode(Modes.VI)).toStrictEqual(['A#', 'C', 'C#', 'D#', 'F', 'F#', 'G#'])
+  expect(scale.mode(Modes.VII)).toStrictEqual(['C', 'C#', 'D#', 'F', 'F#', 'G#', 'A#'])
+  expect(scale.mode(Modes.VIII)).toStrictEqual(['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C'])
+})
+
