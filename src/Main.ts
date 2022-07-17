0cacf921d9c8ad1d5cdca6faca3ece28188f4ee0
simonppg <simonppg@gmail.com>
2022-07-16 23:18:48 -0700

    feat(main): Export Chord as part of the main module

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,8 @@
 import { Note } from './Note'
 import { Scale } from './Scale'
 import { Scales } from './Scales'
+import { Chord } from './Chord'
 
 export {
-  Note, Scale, Scales
+  Note, Scale, Scales, Chord
 }
