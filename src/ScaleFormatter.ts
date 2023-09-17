bbc4801e6aa968f566d8adf9c58218f0f20c15e2
simonppg <simonppg@gmail.com>
2023-09-17 16:59:50 -0700

    Refactor

--- src/ScaleFormatter.ts
+++ src/ScaleFormatter.ts
@@ -6,7 +6,7 @@ export class ScaleFormatter {
   toIntervalPattern(scaleCode: number): number[] {
     let binaryValue = this.binary.toBinary(scaleCode)
 
-    if(binaryValue.length < 12) {
+    if (binaryValue.length < 12) {
       binaryValue = binaryValue.toString().padStart(12, '0')
     }
 
@@ -16,17 +16,17 @@ export class ScaleFormatter {
     let numOfIntervals = 1;
     for (let i = 0; i < binaryValue.length; i++) {
       const current = binaryValue.charAt(i)
-      if(current == '1') {
+      if (current == '1') {
         res.push(numOfIntervals)
         numOfIntervals = 1
       }
-      else{
+      else {
         numOfIntervals++
       }
     }
 
     const num = res.shift()
-    if(num != undefined && num == 1)
+    if (num != undefined && num == 1)
       res.push(numOfIntervals)
 
     return res
@@ -38,27 +38,21 @@ export class ScaleFormatter {
     for (let i = 0; i < intervalPattern.length; i++) {
       const current = intervalPattern[i]
 
-      if(current == 3) {
-        res += '001'
-      } else if(current == 2) {
-        res += '01'
-      } else {
-        res += '1'
-      }
+      const str = '1'.toString().padStart(current, '0')
+
+      res += str
     }
 
     res = res.split("").reverse().join("")
-    
+
     const numArray = res.split("")
     const num = numArray.shift()
-    if(num != undefined)
+    if (num != undefined)
       numArray.push(num)
 
     res = numArray.join("")
 
-    let ares = parseInt(res, 2)
-
-    return ares
+    return this.binary.toDecimal(res)
   }
 
 }
--- src/math/Binary.ts
+++ src/math/Binary.ts
@@ -1,4 +1,8 @@
 export class Binary {
+  toDecimal(res: string): number {
+    return parseInt(res, 2)
+  }
+
   toBinary(decimalNumber: number) {
     return decimalNumber.toString(2);
   }
