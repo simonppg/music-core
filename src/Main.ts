39f1c2eefa2b04922dd6f6e6d51664aa85275655
simonppg <simonppg@gmail.com>
2023-09-17 17:08:32 -0700

    Export ScaleFormatter

--- src/Main.ts
+++ src/Main.ts
@@ -2,7 +2,8 @@ import { Note } from './Note'
 import { Scale } from './Scale'
 import { IntervalPattern } from './IntervalPattern'
 import { Chord } from './Chord'
+import { ScaleFormatter } from './ScaleFormatter'
 
 export {
-  Note, Scale, IntervalPattern, Chord
+  Note, Scale, IntervalPattern, Chord, ScaleFormatter
 }
