64455c541f358768ff997daa5592889143bbf82c
simonppg <simonppg@gmail.com>
2023-09-23 11:50:04 -0700

    Tests scales modes

--- src/IntervalPattern.ts
+++ src/IntervalPattern.ts
@@ -1,5 +1,13 @@
 export const IntervalPattern = {
   mayor: [2, 2, 1, 2, 2, 2, 1],
   blues: [3, 2, 1, 1, 3, 2],
-  pentatonic: [2, 2, 3, 2, 3]
+  pentatonic: [2, 2, 3, 2, 3],
+
+  ionian:     [2, 2, 1, 2, 2, 2, 1],
+  dorian:     [2, 1, 2, 2, 2, 1, 2],
+  phrygian:   [1, 2, 2, 2, 1, 2, 2],
+  lydian:     [2, 2, 2, 1, 2, 2, 1],
+  mixolydian: [2, 2, 1, 2, 2, 1, 2],
+  aeolian:    [2, 1, 2, 2, 1, 2, 2],
+  locrian:    [1, 2, 2, 1, 2, 2, 2]
 } as const
